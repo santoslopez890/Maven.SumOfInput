8200a782fa954136502ee87a7cd1905404fea8a3
Santos <santos@Zipcoders-MacBook-Pro-9.local>
2023-06-28 18:43:41 -0400

    finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,12 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args){
-        
+
         int number = getInputNumber();
         
         int sum = sumOfNumbers(number);
@@ -15,12 +17,16 @@ public class Main {
     
     // do this one and the lab is complete. really.
     // return the sum of 0 to n... if n == 3, the result should be 6
-    int sumOfNumbers(int n) {
-        return 0;
+    static int sumOfNumbers(int n) {
+        int sum=0;
+        for(int i=0;i<=n;i++){
+            sum+=i;
+        }
+        return sum;
     }
     
     
-    int getInputNumber() {
+    static int getInputNumber() {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter any number: ");
 
